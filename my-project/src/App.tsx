f22ee525340fc3a1c99c3549945068f7a409eae9
MaiQuocHuy <maiquochuy16122003@gmail.com>
2025-06-30 14:59:01 +0700

    feat(ui): add block ui element

---
+++ my-project/src/App.tsx
@@ -0,0 +1,29 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+
+  return (
+    // Create a resume page with a header, a main content, and a footer in the main page separate two layout column 1 display personal information and 2 display education and experience
+    <>
+      <header>
+        <h1>Resume</h1>
+      </header>
+      <main>
+        <div>
+          
+        </div>
+        <div>
+          
+        </div>
+      </main>
+      <footer>
+        <p>Copyright 2025</p>
+      </footer>
+    </>
+  );
+}
+
+export default App;
