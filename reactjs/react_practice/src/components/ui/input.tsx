e1f495040b62a02eb41adf37190ab0a09eb3927b
MaiQuocHuy <maiquochuy16122003@gmail.com>
2025-07-02 18:55:43 +0700

    chore: update css in input component

--- reactjs/react_practice/src/components/ui/input.tsx
+++ reactjs/react_practice/src/components/ui/input.tsx
@@ -1,22 +1,18 @@
 import React from "react";
 
-type InputProps = {
-  placeholder?: string;
-  className?: string;
-  value?: string;
-  fontWeight?: "normal" | "bold" | "semibold";
-  iconLeft?: React.ReactNode;
-  iconRight?: React.ReactNode;
-};
-
 const Input = ({
   placeholder = "",
   className = "",
-  value = "",
   fontWeight = "normal",
   iconLeft,
   iconRight,
-}: InputProps) => {
+}: {
+  placeholder?: string;
+  className?: string;
+  fontWeight?: "normal" | "bold" | "semibold";
+  iconLeft?: React.ReactNode;
+  iconRight?: React.ReactNode;
+}) => {
   return (
     <div
       className={`p-2 py-1 border border-gray-300 rounded-2xl flex items-center bg-white shadow-sm hover:shadow-md transition-shadow duration-300 w-xs max-w-md ${className}`}
@@ -25,7 +21,6 @@ const Input = ({
       <input
         type="text"
         placeholder={placeholder}
-        value={value}
         className={`p-2 border-none rounded-lg focus:outline-none w-full font-${fontWeight}`}
       />
       {iconRight && <div className="ml-2">{iconRight}</div>}
