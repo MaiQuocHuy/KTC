42628d6c6ace0ea8a2139141f64b52d17bd9ea94
MaiQuocHuy <maiquochuy16122003@gmail.com>
2025-07-08 17:49:06 +0700

    refactor: remove unused imports in DepartmentsPage component for cleaner code

--- reactjs/react_practice_homework_8_7/src/pages/DepartmentsPage.tsx
+++ reactjs/react_practice_homework_8_7/src/pages/DepartmentsPage.tsx
@@ -1,9 +1,6 @@
-import notification from "../assets/notification-bell-svgrepo-com.svg";
-import search from "../assets/search-svgrepo-com.svg";
 const DepartmentsPage = () => {
   return (
     <div>
-
       <h1 className="text-2xl font-semibold">Departments Page</h1>
     </div>
   );
